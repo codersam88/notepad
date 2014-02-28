86db7a98da3f82906afcae977949f289c513a2ce
sampath <sampath.surineni@gmail.com>
2014-03-01 02:51:15 +0530

    ui started

---
+++ src/notepad/UIBuilder.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package notepad;
+
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+
+/**
+ *
+ * @author sampath
+ */
+public class UIBuilder {
+    private JFrame mainWind;
+    private JTextArea paper;
+
+    public UIBuilder() {
+        mainWind = new JFrame();
+        paper = new JTextArea();
+        mainWind.add(paper);
+        mainWind.pack();
+        mainWind.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainWind.setVisible(true);
+    }
+    
+    
+    
+}
